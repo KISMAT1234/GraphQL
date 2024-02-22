c021269b23ae297380f81b04f14d85fb958ae04a
KISMAT1234 <kismatbohora123@gmail.com>
2024-02-22 23:46:19 +0545

    server runned

--- quote/server.js
+++ quote/server.js
@@ -5,10 +5,22 @@ const typeDefs = gql`
 type Query{
    greet:String
 }
+`
 
 const resolvers={
-     
+     Query:{
+        greet:()=>"hello world"
+     }
 }
 
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+})
 
-`
+
+server.listen().then(({url})=>{
+    console.log(`server running at ${url}`);
+})
+
+
