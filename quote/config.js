8a1f7052bb4009368b3580351ecd0ff12ad65424
KISMAT1234 <kismatbohora123@gmail.com>
2024-02-24 15:45:24 +0545

    package

---
+++ quote/config.js
@@ -0,0 +1,1 @@
+// export const MONGO_URL = mongodb://localhost:27017/graphql
--- quote/server.js
+++ quote/server.js
@@ -1,20 +1,23 @@
 // const express = require ('express');
 import express from 'express'
 import {ApolloServer} from '@apollo/server';
-import { gql } from 'apollo-server-core';
+// import { gql } from 'apollo-server-core';
 import {expressMiddleware} from'@apollo/server/express4'
 import bodyParser from "body-parser"
 import typeDefs from './schemaGql.js';
 import resolvers from './resolvers.js';
+import mongoose from  'mongoose';
+import './models/Quotes.js'
+import './models/User.js'
 
-
+mongoose.connect('mongodb://127.0.0.1:27017/graphql').then(()=>{
+    console.log("connected to the database")
+}).catch((error)=>{
+    console.log('Error:',error.message)
+});
 
 async function startServer() {
     const app = express()
-    
-
-
-
 const server = new ApolloServer({
         typeDefs,
         resolvers,
