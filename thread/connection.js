4a50ec1fc9cdd06a624a48d30d6b58b8b6432a54
KISMAT1234 <kismatbohora123@gmail.com>
2024-03-01 11:00:14 +0545

    database connecton

---
+++ thread/connection.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose'
+
+const databaseConnection = async() =>{
+   try{
+    await mongoose.connect("mongodb://localhost:27017/graphconnect")
+    console.log("Database Connected Successfully");
+   }catch(err){
+       console.error(err);
+       process.exit();
+   }
+}
+
+export default databaseConnection;
+
