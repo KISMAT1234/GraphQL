232f34eb0516d82b2dfd6a3a22ddee4de1c27dfd
KISMAT1234 <kismatbohora123@gmail.com>
2024-03-03 19:28:38 +0545

    data showed successfully in UI using graphql

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,7 @@ query GetTodosWithUser{
 `
 function App() {
   const {data, loading} = useQuery(query)
+  console.log(data);
 
   if(loading) return <h1>Loading...</h1>
 
@@ -16,7 +17,16 @@ function App() {
   return (
     <>
       <div className="App">
-         {JSON.stringify(data)}
+         {/* {JSON.stringify(data)} */}
+         <table>
+            <tbody>
+               {
+                data.allUsers.map(item =>
+                  <td>{item.username}</td>
+                  )
+               }
+            </tbody>
+         </table>
       </div>
     
     </>
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+// import './index.css'
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 const client = new ApolloClient({
