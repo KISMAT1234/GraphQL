8cc7df76c4b4e1a0f945eaf29b1a74c7ca224a33
KISMAT1234 <kismatbohora123@gmail.com>
2024-03-20 19:46:34 +0545

    calling apollogqlserver to split code

--- thread/index.js
+++ thread/index.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import { ApolloServer } from '@apollo/server';
+// import { ApolloServer } from '@apollo/server';
 import {expressMiddleware} from '@apollo/server/express4'
-import users from "./model.js"
-import databaseConnection from './connection.js'
-import cors from 'cors'
+// import users from "./model.js"
+import createApolloGraphqlServer from "./graphql/index.js"
+// import databaseConnection from './connection.js'
+// import cors from 'cors'
 
 
 
@@ -14,54 +15,54 @@ const PORT = Number(process.env.PORT) || 8000
 
 app.use(express.json())
 
-app.use(cors());
+// app.use(cors());
 
-databaseConnection()
+// databaseConnection()
 // graphql server
 
-const gqlServer = new ApolloServer({
-    typeDefs:`
-    type User{
-       username:String!
-       email: String!
-       password: String!
-    }
+// const gqlServer = new ApolloServer({
+//     typeDefs:`
+//     type User{
+//        username:String!
+//        email: String!
+//        password: String!
+//     }
 
-     type Query{
-         allUsers:[User]
-     }
+//      type Query{
+//          allUsers:[User]
+//      }
 
-     type Mutation {
-        createUser(username: String!, email: String!, password: String!): User!
-      }
+//      type Mutation {
+//         createUser(username: String!, email: String!, password: String!): User!
+//       }
      
   
     
  
-    `,  //Schema
-    resolvers:{
-        Query:{
-            allUsers: async()=>{
-                return await users.find()
-            },
-        },
-        Mutation: {
-            createUser: async (_, { username, email,password }) => {
-              const user = new users({ username, email,password });
-              await user.save();
-              return user;
-            },
-          },
-    }  
-});
+//     `,  //Schema
+//     resolvers:{
+//         Query:{
+//             allUsers: async()=>{
+//                 return await users.find()
+//             },
+//         },
+//         Mutation: {
+//             createUser: async (_, { username, email,password }) => {
+//               const user = new users({ username, email,password });
+//               await user.save();
+//               return user;
+//             },
+//           },
+//     }  
+// });
 
-await gqlServer.start()
+// await gqlServer.start()
 
 app.get('/',(req,res)=>{
   res.end("Kismat good boy")
 })
 
-app.use('/graphql',expressMiddleware(gqlServer))
+app.use('/graphql',expressMiddleware(await createApolloGraphqlServer()));
 
 app.listen( PORT,()=>{
     console.log(`Server is running on port ${PORT}`)
