32b69505fc1eb49441a1579d7d9bbab4782415fc
KISMAT1234 <kismatbohora123@gmail.com>
2024-03-20 19:49:03 +0545

    main file for user where all required method are store in User

---
+++ thread/graphql/user/index.js
@@ -0,0 +1,6 @@
+import {typeDefs} from './typedef.js'
+import {queries} from  './queries.js'
+import {resolvers} from './resolvers.js'
+import {mutations} from './mutations.js'
+
+export const User = {typeDefs, queries, mutations, resolvers}
