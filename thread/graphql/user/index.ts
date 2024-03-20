fa85e5365beabb2c57209c793fecb0d6e3ab8201
KISMAT1234 <kismatbohora123@gmail.com>
2024-03-20 19:45:34 +0545

    changing file from ts to js

--- thread/graphql/user/index.ts
+++
@@ -1,6 +0,0 @@
-import {typeDefs} from "./typedef"
-import {queries} from  './queries'
-import {mutation} from './resolvers'
-import {resolvers} from './mutation'
-
-export const User = {typeDefs, queries, mutation, resolvers}
--- thread/graphql/user/resolvers.ts
+++
@@ -1,2 +0,0 @@
-const queris = {} 
-const mutation = {}
